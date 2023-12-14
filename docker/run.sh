fe2199a73776218b497515aea76dde170644e243
pb_BD <pb_BD>
2023-12-14 13:41:59 +0100

    fix entry

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-composer install && php bin/console doctrine:mongodb:schema:create && chmod -R 777 /var/www/html/var && chmod -R 777 /var/www/html/public/photos
+composer install && chmod -R 777 /var/www/html/var && chmod -R 777 /var/www/html/public/photos && php bin/console doctrine:mongodb:schema:create
 apache2-foreground
 
